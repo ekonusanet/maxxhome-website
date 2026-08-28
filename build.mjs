28c28dbf47ff3771bc637cfa3955b5522a248446
ekonusanet <133492652+ekonusanet@users.noreply.github.com>
2026-08-28 20:57:24 +0700

    Add build.mjs

---
+++ build.mjs
@@ -0,0 +1,9 @@
+import { cp, mkdir, rm } from 'node:fs/promises';
+
+await rm('dist', { recursive: true, force: true });
+await mkdir('dist/client/assets', { recursive: true });
+await mkdir('dist/server', { recursive: true });
+await cp('index.html', 'dist/client/index.html');
+await cp('assets', 'dist/client/assets', { recursive: true });
+await cp('worker.js', 'dist/server/index.js');
+
